89d404e3bc363a54eda58424927af464a2b7f150
Nanda Hermawansyah <95904979+nandacjo@users.noreply.github.com>
2023-05-21 10:02:07 +0700

    first commit

---
+++ app/Http/Controllers/MasjidController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Masjid;
+use App\Http\Requests\StoreMasjidRequest;
+use App\Http\Requests\UpdateMasjidRequest;
+
+class MasjidController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMasjidRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Masjid $masjid)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Masjid $masjid)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMasjidRequest $request, Masjid $masjid)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Masjid $masjid)
+    {
+        //
+    }
+}
