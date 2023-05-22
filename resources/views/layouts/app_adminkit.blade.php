2c94e10a6aeb20982dd740e5c3cea27cbf967518
Nanda Hermawansyah <95904979+nandacjo@users.noreply.github.com>
2023-05-22 11:24:25 +0700

    Menambahkan gambar avatar default di profil

--- resources/views/layouts/app_adminkit.blade.php
+++ resources/views/layouts/app_adminkit.blade.php
@@ -298,20 +298,13 @@
 
                             <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#"
                                 data-bs-toggle="dropdown">
-                                <img src="img/avatars/avatar.jpg" class="avatar img-fluid rounded me-1"
-                                    alt="Charles Hall" /> <span class="text-dark">Charles Hall</span>
+                                <img src="{{ asset('images/avatar.png') }}" class="avatar img-fluid rounded me-1"
+                                    alt="{{ auth()->user()->name }}" /> <span
+                                    class="text-dark">{{ auth()->user()->name }}</span>
                             </a>
                             <div class="dropdown-menu dropdown-menu-end">
                                 <a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1"
-                                        data-feather="user"></i> Profile</a>
-                                <a class="dropdown-item" href="#"><i class="align-middle me-1"
-                                        data-feather="pie-chart"></i> Analytics</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="index.html"><i class="align-middle me-1"
-                                        data-feather="settings"></i> Settings & Privacy</a>
-                                <a class="dropdown-item" href="#"><i class="align-middle me-1"
-                                        data-feather="help-circle"></i> Help Center</a>
-                                <div class="dropdown-divider"></div>
+                                        data-feather="user"></i> Ubah Profil</a>
                                 <a class="dropdown-item" href="{{ route('logout-user') }}">Log out</a>
                             </div>
                         </li>
