88020cfa06cfa7d4bcc4ba9a4ab776b3e0b9eae5
Nanda Hermawansyah <95904979+nandacjo@users.noreply.github.com>
2023-06-01 23:13:42 +0700

    Menambahkan tabel profil masjid

---
+++ app/Http/Controllers/ProfilController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Profil;
+use App\Http\Requests\StoreProfilRequest;
+use App\Http\Requests\UpdateProfilRequest;
+
+class ProfilController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProfilRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Profil $profil)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Profil $profil)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProfilRequest $request, Profil $profil)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Profil $profil)
+    {
+        //
+    }
+}
