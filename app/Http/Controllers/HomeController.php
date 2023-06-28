f75ee3f4a89236527d222c3d62e146b92209a3fa
Nanda <nandacoje22@gmail.com>
2023-06-28 23:42:00 +0700

    Membuat halaman dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Infaq;
+use App\Models\Kas;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('home');
-    }
+  /**
+   * Show the application dashboard.
+   *
+   * @return \Illuminate\Contracts\Support\Renderable
+   */
+  public function index()
+  {
+    $data['saldoAkhir']  = Kas::SaldoAkhir();
+    $data['totalInfaq'] = Infaq::userMasjid()->whereDate('created_at', now()->format('Y-m-d'))->sum('jumlah');
+    $data['kas'] = Kas::userMasjid()->latest()->take(7)->get();
+    return view('home', $data);
+  }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,17 +3,125 @@
 @section('title', 'Beranda')
 
 @section('content')
-    <h1 class="h3 mb-3">Beranda E-Masjid</h1>
+  <div class="container-fluid p-0">
+
+    <h1 class="h3 mb-3"><strong>Analytics</strong> Dashboard</h1>
 
     <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title mb-0">Selamat Datang {{ auth()->user()->name }}</h5>
-                </div>
+      <div class="col-xl-6 col-xxl-5 d-flex">
+        <div class="w-100">
+          <div class="row">
+            <div class="col-12">
+              <div class="card">
                 <div class="card-body">
+                  <div class="row">
+                    <div class="col mt-0">
+                      <h5 class="card-title">Saldo Akhir</h5>
+                    </div>
+
+                    <div class="col-auto">
+                      <div class="stat text-primary">
+                        <i class="align-middle" data-feather="truck"></i>
+                      </div>
+                    </div>
+                  </div>
+                  <h1 class="mt-1 mb-3">{{ formatRupiah($saldoAkhir, true) }}</h1>
+                  <div class="mb-0">
+                    <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -3.65% </span>
+                    <span class="text-muted">Since last week</span>
+                  </div>
                 </div>
+              </div>
             </div>
+            <div class="col-12">
+              <div class="card">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col mt-0">
+                      <h5 class="card-title">Total Infaq Hari Ini</h5>
+                    </div>
+
+                    <div class="col-auto">
+                      <div class="stat text-primary">
+                        <i class="align-middle" data-feather="dollar-sign"></i>
+                      </div>
+                    </div>
+                  </div>
+                  <h1 class="mt-1 mb-3">{{ formatRupiah($totalInfaq, true) }}</h1>
+                  <div class="mb-0">
+                    <span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> 6.65% </span>
+                    <span class="text-muted">Since last week</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+
+      <div class="col-xl-6 col-xxl-7">
+        <div class="card flex-fill w-100">
+          <div class="card-header">
+            <h5 class="card-title mb-0">Recent Movement</h5>
+          </div>
+          <div class="card-body py-3">
+            <div class="chart chart-sm">
+              <canvas id="chartjs-dashboard-line"></canvas>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
+
+    <div class="row">
+      <div class="col-12 col-lg-8 col-xxl-9 d-flex">
+        <div class="card flex-fill">
+          <div class="card-header">
+
+            <h5 class="card-title mb-0">Latest Projects</h5>
+          </div>
+          <table class="table table-hover my-0">
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>Keterangan</th>
+                <th class="d-none d-xl-table-cell">Tanggal Transaksi</th>
+                <th class="d-none d-xl-table-cell">Jenis</th>
+                <th>Jumlah</th>
+              </tr>
+            </thead>
+            <tbody>
+              @forelse ($kas as $item)
+                <tr>
+                  <td>{{ $loop->iteration }}</td>
+                  <td>{{ $item->keterangan }}</td>
+                  <td class="d-none d-xl-table-cell">{{ $item->tanggal->translatedFormat('l, d-m-Y') }}</td>
+                  <td><span class="badge bg-success">{{ $item->jenis }}</span></td>
+                  <td class="d-none d-xl-table-cell">{{ $item->jumlah }}</td>
+                </tr>
+              @empty
+                <td colspan="5">Data kas tidak ada</td>
+              @endforelse
+
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div class="col-12 col-lg-4 col-xxl-3 d-flex">
+        <div class="card flex-fill w-100">
+          <div class="card-header">
+
+            <h5 class="card-title mb-0">Monthly Sales</h5>
+          </div>
+          <div class="card-body d-flex w-100">
+            <div class="align-self-center chart chart-lg">
+              <canvas id="chartjs-dashboard-bar"></canvas>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
 @endsection
