01a75147bf3beb4386167cd2150ce7d7a604df93
Nanda Hermawansyah <95904979+nandacjo@users.noreply.github.com>
2023-07-18 19:33:10 +0700

    Membuat slug otomatis

--- app/Models/Masjid.php
+++ app/Models/Masjid.php
@@ -2,12 +2,25 @@
 
 namespace App\Models;
 
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Masjid extends Model
 {
-    use HasFactory;
+  use HasFactory;
+  use HasSlug;
 
-    protected $guarded = [];
+  protected $guarded = [];
+
+  /**
+   * Get the options for generating the slug.
+   */
+  public function getSlugOptions(): SlugOptions
+  {
+    return SlugOptions::create()
+      ->generateSlugsFrom('name')
+      ->saveSlugsTo('slug');
+  }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,43 +9,44 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
+  use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'role'
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'name',
+    'email',
+    'password',
+    'role'
+  ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
 
-    public function masjid(): BelongsTo
-    {
-        return $this->belongsTo(Masjid::class);
-    }
+  public function masjid(): BelongsTo
+  {
+    return $this->belongsTo(Masjid::class);
+  }
 }
